fdc67dc093a7605adfd6b8d276518929b8f49953
yonox270 <miguelpereira@yonox-contact.store>
2026-02-27 01:36:38 +0100

    fix: use express adapter for vercel

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import { createRequestHandler } from "@react-router/express";
+import express from "express";
 import * as build from "../build/server/index.js";
 
-export default createRequestHandler({ build });
+const app = express();
+
+app.use(
+  createRequestHandler({
+    build,
+    mode: process.env.NODE_ENV,
+  })
+);
+
+export default app;
