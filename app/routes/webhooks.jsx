5614efd196a717806a16b6770752649ef7fb40a2
yonox270 <miguelpereira@yonox-contact.store>
2026-03-06 16:52:25 +0100

    fix: manual HMAC verification

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -1,12 +1,19 @@
-import { authenticate } from "../shopify.server";
+import crypto from "crypto";
 
 export const action = async ({ request }) => {
-  try {
-    await authenticate.webhook(request);
-    return new Response("OK", { status: 200 });
-  } catch (error) {
+  const hmac = request.headers.get("X-Shopify-Hmac-Sha256");
+  const body = await request.text();
+  
+  const hash = crypto
+    .createHmac("sha256", process.env.SHOPIFY_API_SECRET)
+    .update(body, "utf8")
+    .digest("base64");
+
+  if (hash !== hmac) {
     return new Response("Unauthorized", { status: 401 });
   }
+
+  return new Response("OK", { status: 200 });
 };
 
 export const loader = async () => {
