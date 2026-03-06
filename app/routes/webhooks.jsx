678f468c5af65831f671f061a44ce77f6591e5d4
yonox270 <miguelpereira@yonox-contact.store>
2026-03-06 16:47:13 +0100

    fix: validate HMAC in webhook handler

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -1,7 +1,14 @@
+import { authenticate } from "../shopify.server";
+
 export const action = async ({ request }) => {
-  return new Response("OK", { status: 200 });
+  try {
+    await authenticate.webhook(request);
+    return new Response("OK", { status: 200 });
+  } catch (error) {
+    return new Response("Unauthorized", { status: 401 });
+  }
 };
 
-export const loader = async ({ request }) => {
+export const loader = async () => {
   return new Response("OK", { status: 200 });
 };
