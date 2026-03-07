189975ad3c683f5e669d952ad7f8e29360953311
yonox270 <miguelpereira@yonox-contact.store>
2026-03-07 15:10:54 +0100

    Serve static assets from build/client

--- api/server.js
+++ api/server.js
@@ -3,5 +3,11 @@ import express from "express";
 import * as build from "../build/server/index.js";
 
 const app = express();
+
+// Servir les fichiers statiques
+app.use(express.static("build/client", { maxAge: "1y" }));
+
+// Handler React Router
 app.use(createRequestHandler({ build }));
+
 export default app;
